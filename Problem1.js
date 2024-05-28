d4330bf09c1c27f2f8ffcae915d45c61735e72d9
Samuel Lam <samuellam123456@hotmail.com>
2024-05-28 18:04:41 +0800

    Update Problem1.js

--- Problem1.js
+++ Problem1.js
@@ -1,13 +1,21 @@
 var sum_to_n_a = function(n) {
-    
+    // For loop, O(n)
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+    return sum;
 };
 
 var sum_to_n_b = function(n) {
-    // your code here
+    // Summation = n(n+1)/2, O(1)
+    return (n * (n + 1)) / 2;
 };
 
 var sum_to_n_c = function(n) {
-    // your code here
+    // Recursion, 
+    if (n === 1) {
+    return 1;
+    }
+    return n + sum_to_n(n - 1);
 };
-
-console.log("HI");
