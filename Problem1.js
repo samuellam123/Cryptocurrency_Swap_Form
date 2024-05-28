8c3133d1d2dacb019d95beb84e8f4202970424fc
Samuel Lam <samuellam123456@hotmail.com>
2024-05-28 17:53:29 +0800

    Create Problem1.js

---
+++ Problem1.js
@@ -0,0 +1,13 @@
+var sum_to_n_a = function(n) {
+    
+};
+
+var sum_to_n_b = function(n) {
+    // your code here
+};
+
+var sum_to_n_c = function(n) {
+    // your code here
+};
+
+console.log("HI");
