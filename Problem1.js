1462a0c24c9e791729e4cac8a93a9bef3250c026
Samuel Lam <samuellam123456@hotmail.com>
2024-05-28 19:13:02 +0800

    commit Problem1

--- Problem1.js
+++ Problem1.js
@@ -1,5 +1,10 @@
 var sum_to_n_a = function(n) {
-    // For loop, O(n)
+    // Summation = n(n+1)/2; O(1) time, O(1) space
+    return (n * (n + 1)) / 2;
+};
+
+var sum_to_n_b = function(n) {
+    // For loop; O(n) time, O(1) space
     let sum = 0;
     for (let i = 1; i <= n; i++) {
       sum += i;
@@ -7,15 +12,15 @@ var sum_to_n_a = function(n) {
     return sum;
 };
 
-var sum_to_n_b = function(n) {
-    // Summation = n(n+1)/2, O(1)
-    return (n * (n + 1)) / 2;
-};
-
 var sum_to_n_c = function(n) {
-    // Recursion, 
-    if (n === 1) {
-    return 1;
+    //Dynamic Programming, avoid redundant calculations; O(n) time, O(n) space
+    if (n <= 0) return 0;
+
+    let dp = new Array(n + 1).fill(0);
+  
+    for (let i = 1; i <= n; i++) {
+      dp[i] = dp[i - 1] + i;
     }
-    return n + sum_to_n(n - 1);
+  
+    return dp[n];
 };
